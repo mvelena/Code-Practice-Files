96d5d432a5a72483e984a93ab5b499bcf5220f3f
Elena M <elenavmala@gmail.com>
2022-05-05 18:41:55 -0500

    Clean code

--- script.js
+++ script.js
@@ -2,18 +2,14 @@
 document.body.append(document.createElement("textarea"));
 document.body.append(document.createElement("button"));
 const button = document.querySelector("button");
-console.log(button);
 button.addEventListener("click", function () {
   const input = document.querySelector("textarea").value;
   document.querySelector("textarea").value = "";
-  console.log("value of input", typeof input, input);
   let items = input.split("\n");
-  console.log(items);
-  for (let [i, item] of items.entries()) {
+  for (const [i, item] of items.entries()) {
     if (item != " ") {
       const [first, second] = item.trim().toLowerCase().split("_");
       const output = `${first}${second[0].toUpperCase() + second.slice(1)}`;
-      console.log(output);
       const check = "✔";
       document.querySelector("textarea").value += `${output.padEnd(
         20
